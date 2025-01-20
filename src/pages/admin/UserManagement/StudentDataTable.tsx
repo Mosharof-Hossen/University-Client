7ef43ea76140521902603c6ac2ec3eca3c9782a6
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2025-01-21 02:00:40 +0600

    Handle pagination component

--- src/pages/admin/UserManagement/StudentDataTable.tsx
+++ src/pages/admin/UserManagement/StudentDataTable.tsx
@@ -1,4 +1,4 @@
-import { Button, Space, Table, TableColumnsType, TableProps } from "antd";
+import { Button, Pagination, Space, Table, TableColumnsType, TableProps } from "antd";
 import { useState } from "react";
 import { TQueryParams } from "../../../types/global";
 import { useGetAllStudentsQuery } from "../../../redux/features/admin/userManagement";
@@ -21,7 +21,7 @@ const columns: TableColumnsType<DataType> = [
     },
     {
         title: 'Action',
-        render:()=>(
+        render: () => (
             <Space>
                 <Button>Update</Button>
                 <Button>Details</Button>
@@ -35,17 +35,24 @@ const columns: TableColumnsType<DataType> = [
 
 const StudentDataTable = () => {
     const [params, setParams] = useState<TQueryParams[]>([]);
-    const { data: students, isFetching } = useGetAllStudentsQuery(params);
-    const tableData = students?.data?.map(({ _id, fullName,id}) => ({
-        key: _id, name:fullName,id, 
+    const [page, setPage] = useState<number>(1)
+    const { data: students, isFetching } = useGetAllStudentsQuery([
+        { name: "limit", value: 10 }, 
+        { name: "page", value: page }, 
+        { name: "sort", value: "id" }, 
+
+        ...params]);
+    const tableData = students?.data?.map(({ _id, fullName, id }) => ({
+        key: _id, name: fullName, id,
 
     }))
-    console.log(students);
+    const meta = students?.meta;
+    console.log(meta);
 
     const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
         console.log('params', { filters }, { extra });
         if (extra.action === "filter") {
-            const queryParams:TQueryParams[] = [];
+            const queryParams: TQueryParams[] = [];
 
             filters.name?.forEach((item) => {
                 queryParams.push({ name: "name", value: item });
@@ -55,13 +62,24 @@ const StudentDataTable = () => {
         }
     };
     return (
-        <Table<DataType>
-            loading={isFetching}
-            columns={columns}
-            dataSource={tableData}
-            onChange={onChange}
-            showSorterTooltip={{ target: 'sorter-icon' }}
-        />
+        <div>
+            <Table<DataType>
+                loading={isFetching}
+                columns={columns}
+                dataSource={tableData}
+                onChange={onChange}
+                showSorterTooltip={{ target: 'sorter-icon' }}
+                pagination={false}
+            />
+            <Pagination
+
+                pageSize={meta?.limit}
+                total={meta?.total}
+                onChange={(value) =>setPage(value)}
+                align="center"
+                style={{marginTop:"20px"}}
+            ></Pagination>
+        </div>
     );
 };
 
