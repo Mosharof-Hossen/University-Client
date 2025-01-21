c73cd09004cc19486316eba3d02343d54b562998
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2025-01-22 01:47:27 +0600

    handle status change using antd dropdown

--- src/pages/admin/Course Management/RegisteredSemesters.tsx
+++ src/pages/admin/Course Management/RegisteredSemesters.tsx
@@ -1,59 +1,95 @@
 
-import { Button, Pagination, Space, Table, TableColumnsType, TableProps } from "antd";
-import { useState } from "react";
-import { TQueryParams } from "../../../types/global";
-import { useGetAllStudentsQuery } from "../../../redux/features/admin/userManagement";
-import { Link, NavLink } from "react-router-dom";
+import { Button, Dropdown, Space, Table, TableColumnsType, TableProps, Tag } from "antd";
+
 import { useGetAllRegisteredSemestersQuery } from "../../../redux/features/admin/courseManagement.api";
+import moment from "moment";
 
 
 interface DataType {
     key: string,
     name: string,
-    id: string,
-    email: string,
+    status: string,
+    startDate: string,
+    endDate: string,
 }
 
-const columns: TableColumnsType<DataType> = [
+const items = [
     {
-        title: 'Name',
-        dataIndex: 'name',
+        label:"Upcoming",
+        key:"UPCOMING"
     },
     {
-        title: 'Status',
-        dataIndex: 'status',
+        label:"Ongoing",
+        key:"ONGOING"
     },
     {
-        title: 'Start Date',
-        dataIndex: 'startDate',
+        label:"Ended",
+        key:"ENDED"
     },
-    {
-        title: 'End Date',
-        dataIndex: 'endDate',
-    },
-    
-    {
-        title: 'Action',
-        render: (item) => {
-            console.log(item);
-            return (<Space>
-                <Button>Details</Button>
-                <Button>Block</Button>
-            </Space>)
-        }
-    },
-];
-
+]
 
 
 const RegisteredSemesters = () => {
     const { data: registeredSemesters, isFetching } = useGetAllRegisteredSemestersQuery([]);
     console.log(registeredSemesters);
     const tableData = registeredSemesters?.data?.map(({ _id, academicSemester, status, startDate, endDate }) => ({
-        key: _id, name: academicSemester.name, status, startDate, endDate
+        key: _id, name: academicSemester.name, status,
+        startDate: moment(new Date(startDate)).format("MMMM"),
+        endDate: moment(new Date(endDate)).format("MMMM")
 
     }))
+    const columns: TableColumnsType<DataType> = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+        },
+        {
+            title: 'Status',
+            dataIndex: 'status',
+            render: (item) => {
+                let color = "blue";
+                if (item === "UPCOMING") {
+                    color = "blue"
+                }
+                if (item === "ONGOING") {
+                    color = "green"
+                }
+                if (item === "ENDED") {
+                    color = "red"
+                }
+    
+                return <Tag color={color}>{item}</Tag>
+            }
+        },
+        {
+            title: 'Start Date',
+            dataIndex: 'startDate',
+        },
+        {
+            title: 'End Date',
+            dataIndex: 'endDate',
+        },
+    
+        {
+            title: 'Action',
+            render: (item) => {
+                console.log(item);
+                return (<Tag>
+                    <Dropdown menu={menuProps} placement="bottom" arrow>Update</Dropdown>
+                </Tag>)
+            }
+        },
+    ];
+    
 
+    const handleStatus = (data)=>{
+        console.log(data);
+    }
+
+    const menuProps = {
+        items,
+        onClick:  handleStatus ,
+      };
     const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
 
     };
